d2254434a4d3c907318d95e6e3531fd095ab3067
sol <hskim6521@naver.com>
2021-12-13 21:13:47 +0900

    Add Project "I Am Rich"

---
+++ Udemy_The complete iOS App Development Bootcamp Course/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 김한솔 on 2021/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
