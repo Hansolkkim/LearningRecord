1f63fd65ffa38da96cf7dbceea2ce67994ac2356
sol <hskim6521@naver.com>
2021-12-14 15:35:14 +0900

    Add Project "I Am Poor"

---
+++ Udemy_The complete iOS App Development Bootcamp Course/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 김한솔 on 2021/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
