393b108feed29da6575f2114e53c52060b55d2bb
sol <hskim6521@naver.com>
2021-12-15 16:07:59 +0900

    Add Project "Dicee"

---
+++ Udemy_The complete iOS App Development Bootcamp Course/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by 김한솔 on 2021/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
+    
+
+}
+
