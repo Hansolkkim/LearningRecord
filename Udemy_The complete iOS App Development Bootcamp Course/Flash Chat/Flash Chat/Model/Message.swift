2f3731c4d7fa9db75dbf5882ffb6d8087d1fff4d
sol <hskim6521@naver.com>
2021-12-24 17:06:10 +0900

    Refactor "Flash Chat" Project
    Using Firebase DB, Save the messages(including Sender's email, body of the message) user's sending
    To show message, use UITableView
    Customize UITableViewCell

---
+++ Udemy_The complete iOS App Development Bootcamp Course/Flash Chat/Flash Chat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by 김한솔 on 2021/12/24.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //발신자
+    let body: String //Body of the message
+}
---
+++ Udemy_The complete iOS App Development Bootcamp Course/Flash Chat/Flash Chat/View/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Flash Chat
+//
+//  Created by 김한솔 on 2021/12/24.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() { //initialize 해주는 메소드 -> this is going to be called when we create a new message cell from the xibFile
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/4
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
